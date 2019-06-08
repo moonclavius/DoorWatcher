d61cf32b907870171f56468412ab5464b551042f
Clavius <51387343+MoonClavius@users.noreply.github.com>
2019-06-09 01:53:44 +0200

    Update face.py

--- Modules/face.py
+++ Modules/face.py
@@ -25,18 +25,18 @@ def facefinder(frame):
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
     rgb_small_frame = small_frame[:, :, ::-1]
 
-	face_locations = face_recognition.face_locations(rgb_small_frame)
-	face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+    face_locations = face_recognition.face_locations(rgb_small_frame)
+    face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-	face_names = []
-	for face_encoding in face_encodings:
-	    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-	    name = "Unknown"
+    face_names = []
+    for face_encoding in face_encodings:
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+        name = "Unknown"
 
-	    face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-	    best_match_index = np.argmin(face_distances)
-	    if matches[best_match_index]:
-		name = known_face_names[best_match_index]
-		print (name)
-		
-	    face_names.append(name)
+        face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+        	name = known_face_names[best_match_index]
+        	print (name)
+
+        face_names.append(name)
