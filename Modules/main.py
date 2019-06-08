9f8587233b60b7d40df0f88ed799dd4d6dd2ca54
Clavius <51387343+MoonClavius@users.noreply.github.com>
2019-06-09 01:31:02 +0200

    Update main.py

--- Modules/main.py
+++ Modules/main.py
@@ -3,37 +3,29 @@ import sys
 import time
 import threading
 
+from face import facefinder
 from camera import VideoCamera
-from face import detectPersonen
-from flask_basicauth import BasicAuth
 from flask import Flask, render_template, Response
 
-video_camera = VideoCamera(flip=True) # creates a camera object, flip vertically
+video_camera = VideoCamera(flip=False)
+object_classifier = cv2.CascadeClassifier("facial_recognition_model.xml")
 
-# App Globals
+
 app = Flask(__name__)
-app.config['BASIC_AUTH_USERNAME'] = 'CHANGE_ME_USERNAME'
-app.config['BASIC_AUTH_PASSWORD'] = 'CHANGE_ME_PLEASE'
-app.config['BASIC_AUTH_FORCE'] = True
-
-basic_auth = BasicAuth(app)
 last_epoch = 0
 
 def check_for_objects():
-	global last_epoch
-	while True:
-		try:
-			frame, found_obj = video_camera.get_object(cv2.CascadeClassifier("facial_recognition_model.xml"))
-			if found_obj and (time.time() - last_epoch) > 600:
-				last_epoch = time.time()
-				#############
-				#detectPersonen(frame)
-				#############
-		except:
-			print ("Error face_recognition: "), sys.exc_info()[0]
+        global last_epoch
+        while True:
+                try:
+                        frame, found_obj = video_camera.get_object(object_classifier)
+                        if found_obj and (time.time() - last_epoch) > 2:
+                                last_epoch = time.time()
+				facefinde(frame)
+                except:
+                        print ("Error finding_face: "), sys.exc_info()[0]
 
 @app.route('/')
-@basic_auth.required
 def index():
     return render_template('index.html')
 
@@ -53,3 +45,4 @@ if __name__ == '__main__':
     t.daemon = True
     t.start()
     app.run(host='0.0.0.0', debug=False)
+
