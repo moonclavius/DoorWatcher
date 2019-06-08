8a9d9210fe2ac4825e888b66f2dbf5c14b6d98d6
Clavius <51387343+MoonClavius@users.noreply.github.com>
2019-06-09 01:41:27 +0200

    Update doorwatcher_software.sh

--- Scripts/doorwatcher_software.sh
+++ Scripts/doorwatcher_software.sh
@@ -14,6 +14,8 @@ sudo sed -i 's/CONF_SWAPSIZE=100/CONF_SWAPSIZE=2048/g' /etc/dphys-swapfile
 sudo /etc/init.d/dphys-swapfile stop
 sudo /etc/init.d/dphys-swapfile start
 sudo pip install numpy dlib
+
+sudo pip3 --no-cache-dir install face_recognition
 sudo pip3 install git+https://github.com/ageitgey/face_recognition_models
 deactivate
 echo "================================================================================================================================"
